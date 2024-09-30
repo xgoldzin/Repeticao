2d39d82ea244a3c797dee64333c086974ffd36fe
xgoldzin <gustafera34@gmail.com>
2024-09-30 17:02:56 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -7,4 +7,28 @@ function tab(){
     }
 }
 
-
+    
+let total = 0
+function calculaProd(){
+    document.getElementById("respostaProdutos").innerHTML = ""
+    let vlrPrd = Number(document.getElementById("prod").value)
+    
+    total += vlrPrd
+    
+    document.getElementById("respostaProdutos").innerHTML = " Valor total:" + total
+    }
+saldo = 0
+function calculaProd(){
+    document.getElementById("respostaProdutos").innerHTML = ""
+    // let sdd = Number(document.getElementById("sld").value)
+    let valorOp = Number(document.getElementById("vlr").value)
+    let operac = document.getElementById("op").value
+        if (operac === "debito"){
+            saldo -= valorOp
+        }
+        else{
+            saldo += valorOp
+        }
+        
+    document.getElementById("respostaBanco").innerHTML = " Valor total:" + saldo
+    }
